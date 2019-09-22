b65845a7a7d9e5ecfd0b867ef326945c3999ba00
Mateo Zabala <mzabala1@eafit.edu.co>
2019-09-21 22:25:46 -0500

    Añado funcionalidad recaptcha

--- apps/usuario/forms.py
+++ apps/usuario/forms.py
@@ -1,8 +1,12 @@
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from snowpenguin.django.recaptcha2.widgets import ReCaptchaWidget
+from snowpenguin.django.recaptcha2.fields import ReCaptchaField
 
 
 class RegistroForm(UserCreationForm):
+    captcha = ReCaptchaField(widget=ReCaptchaWidget(attrs={'classs': 'validate'}), label="")
+
     class Meta:
         model = User
         fields = ['username',
